82c8ba941080ce71798d88a94342a33217062d64
Satyamdepot2020 <105622326+Satyamdepot2020@users.noreply.github.com>
2023-06-06 17:22:59 +0530

    first commit

---
+++ account/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+
+# Create your views here.
+class UserRegistrationView(APIView):
+    def post(self,request,format=None):
+        return Response({'msg':'Registration Done'})
+    
+
+
